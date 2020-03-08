ec3e38ef5acb8a52079b5705a1e9238be0906d18
vikaskatiyar@live.com <vikaskatiyar@live.com>
2020-03-08 14:02:43 +0530

    app.py commit first

---
+++ app.py
@@ -0,0 +1,20 @@
+print("Vikas")
+print('I am from Kanpur')
+print('I am currently living in Pune')
+"""
+This is a comment
+"""
+# Here the indentation is very important
+# if statement
+x = 1
+y = 2
+if x < y:
+    global p
+    p = '\'globalVariable\''
+    print("One is less than Two")
+print("Global Variable P = "+p)
+
+print(10%6)
+
+# print the first 100 prime numbers
+i = 1;
